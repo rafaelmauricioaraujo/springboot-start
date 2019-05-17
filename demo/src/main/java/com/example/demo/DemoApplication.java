f10fe17e9ddaa88832dfb17c18563a692ce938ef
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-05-17 09:12:13 -0300

    create a test controller

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String Hello() {
+		return "Hello World";
+	}
+	
 }
