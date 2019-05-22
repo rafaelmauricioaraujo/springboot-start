4bf8b8048df2268c859d062a567021cc5018b5fc
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-05-22 19:06:38 -0300

    implementando os outros metodos do crud

--- demo/src/main/java/com/example/demo/ProductServiceController.java
+++ demo/src/main/java/com/example/demo/ProductServiceController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,5 +39,18 @@ public class ProductServiceController {
       return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
    }
 
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+   public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) { 
+      productRepo.remove(id);
+      product.setId(id);
+      productRepo.put(id, product);
+      return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
+   }
+
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+   public ResponseEntity<Object> delete(@PathVariable("id") String id) { 
+      productRepo.remove(id);
+      return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+   }
    
 }
